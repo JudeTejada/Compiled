ff260db51064a9111160e4a872866454771de476
Jude <judelaurentetejada@gmail.com>
2021-07-08 20:54:40 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...fontFamily.sans]
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
