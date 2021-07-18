029eaa327490e76a775e84fa4ae1ae6926156536
Jude <judelaurentetejada@gmail.com>
2021-07-18 18:24:13 +0800

    wrap in anchor link

--- components/articleCard/ArticleCard.tsx
+++ components/articleCard/ArticleCard.tsx
@@ -1,4 +1,5 @@
 import NextImage from 'next/image';
+import NextLink from 'next/link';
 
 import { Column } from '@/lib/types';
 
@@ -16,25 +17,23 @@ export const ArticleCard = ({ properties }: Column) => {
 
   const { Description, Link, Image, Name, Category } = properties;
 
-  // console.log(Name.title[0]);
-
-  renderContent(' ');
   return (
-    <div className='p-6 rounded-md cursor-pointer group hover:bg-secondaryDark'>
-      <div className='relative flex-shrink-0 pb-52'>
-        <NextImage
-          src={Image.rich_text[0].plain_text}
-          alt='picture'
-          layout='fill'
-          className='object-cover rounded-md'
-        />
+    <a href={Link.rich_text[0].plain_text} target='_blank' rel='noreferrer'>
+      <div className='p-6 rounded-md cursor-pointer group hover:bg-secondaryDark'>
+        <div className='relative flex-shrink-0 pb-52'>
+          <NextImage
+            src={Image.rich_text[0].plain_text}
+            layout='fill'
+            className='object-cover rounded-md'
+          />
+        </div>
+        <div className='flex flex-col gap-4 mt-6'>
+          <h2 className='text-lg'>{Name.title[0].plain_text}</h2>
+          <p className='leading-relaxed text-secondaryLight'>
+            {renderContent(Description.rich_text[0].plain_text)}
+          </p>
+        </div>
       </div>
-      <div className='flex flex-col gap-4 mt-6'>
-        <h2 className='text-lg'>{Name.title[0].plain_text}</h2>
-        <p className='leading-relaxed text-secondaryLight'>
-          {renderContent(Description.rich_text[0].plain_text)}
-        </p>
-      </div>
-    </div>
+    </a>
   );
 };
