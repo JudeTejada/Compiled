fb11896e2d4bc24eba12d21e85939881ff39f767
Jude <judelaurentetejada@gmail.com>
2021-08-15 20:32:40 +0800

    remove backend

--- server/index.ts
+++ server/index.ts
@@ -1,62 +1,62 @@
-// const express = require('express');
-import express from 'express';
-import next from 'next';
-import { Client } from '@notionhq/client';
+// // const express = require('express');
+// import express from 'express';
+// import next from 'next';
+// import { Client } from '@notionhq/client';
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+// const dev = process.env.NODE_ENV !== 'production';
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
 
-const notion = new Client({
-  auth: 'secret_z4NGj1rP11tR4dgx8aONbjmsPq4gDob7piT7OgoG9MA'
-});
+// const notion = new Client({
+//   auth: 'secret_z4NGj1rP11tR4dgx8aONbjmsPq4gDob7piT7OgoG9MA'
+// });
 
-app.prepare().then(() => {
-  const server = express();
-  server.use(express.json());
+// app.prepare().then(() => {
+//   const server = express();
+//   server.use(express.json());
 
-  server.post('/api/submit', (req: any, res: any) => {
-    const { link, category } = req.body;
+//   server.post('/api/submit', (req: any, res: any) => {
+//     const { link, category } = req.body;
 
-    const database_id = '8b64e3b9f3734474a2581330714172ce';
+//     const database_id = '8b64e3b9f3734474a2581330714172ce';
 
-    try {
-      const response = notion.pages.create({
-        parent: {
-          database_id
-        },
-        properties: {
-          Link: {
-            type: 'title',
-            title: [
-              {
-                type: 'text',
-                text: {
-                  content: link
-                }
-              }
-            ]
-          },
-          category: {
-            select: {
-              name: category
-            },
-            type: 'select'
-          }
-        }
-      });
-      res.status(200).json({ message: 'SUCCESS' });
-    } catch (error) {
-      res.status(400).json({ message: 'something went wrong' });
-    }
-  });
+//     try {
+//       const response = notion.pages.create({
+//         parent: {
+//           database_id
+//         },
+//         properties: {
+//           Link: {
+//             type: 'title',
+//             title: [
+//               {
+//                 type: 'text',
+//                 text: {
+//                   content: link
+//                 }
+//               }
+//             ]
+//           },
+//           category: {
+//             select: {
+//               name: category
+//             },
+//             type: 'select'
+//           }
+//         }
+//       });
+//       res.status(200).json({ message: 'SUCCESS' });
+//     } catch (error) {
+//       res.status(400).json({ message: 'something went wrong' });
+//     }
+//   });
 
-  server.get('*', (req, res) => {
-    return handle(req, res);
-  });
+//   server.get('*', (req, res) => {
+//     return handle(req, res);
+//   });
 
-  server.listen(3000, (err?: any) => {
-    if (err) throw err;
-    console.log('Ready on http://localhost:5000');
-  });
-});
+//   server.listen(3000, (err?: any) => {
+//     if (err) throw err;
+//     console.log('Ready on http://localhost:5000');
+//   });
+// });
