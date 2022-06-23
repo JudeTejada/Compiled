14a071374765a738e84a369e74b711073da56706
Jude <judelaurentetejada@gmail.com>
2022-06-23 21:11:21 +0800

    feat: :art: setup storybooks and created a Button story
    setup storybooks and created a Button story
    
    none

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,7 +1,8 @@
 import NextLink from 'next/link';
+import cls from 'classnames';
 
 interface ButtonProps {
-  type: string;
+  type: 'primary';
   className?: string;
   disabled?: boolean;
 }
@@ -10,19 +11,13 @@ interface ButtonLinkProps extends ButtonProps {
   href: string;
 }
 
-const ButtonSwitchStyles = (type: string, disabled?: boolean) => {
-  const baseStyles = `outline-none cursor-pointer text-base px-3 py-3 ${
-    disabled && 'cursor-not-allowed bg-secondaryLight '
-  }`;
-
-  switch (type) {
-    case 'primary':
-      return ` font-semibold rounded-md  text-white  bg-purple hover:bg-purpleLight focus-visible:bg-purpleLight  ${baseStyles}  `;
-
-    default: {
-      throw new Error('variant not supported for that');
-    }
-  }
+const defaultStyles = {
+  base: 'outline-none cursor-pointer text-base px-3 py-3',
+  disabled: 'cursor-not-allowed bg-secondaryLight'
+};
+const buttonStyles = {
+  primary:
+    'font-semibold rounded-md  text-white  bg-purple hover:bg-purpleLight focus-visible:bg-purpleLight'
 };
 
 export const Button = ({
@@ -33,7 +28,13 @@ export const Button = ({
 }: React.PropsWithChildren<ButtonProps>) => {
   return (
     <button
-      className={` ${ButtonSwitchStyles(type, disabled)} ${className} `}
+      className={cls(
+        defaultStyles.base,
+        buttonStyles[type],
+        disabled && defaultStyles.disabled,
+        { className }
+      )}
+      // className={` ${ButtonSwitchStyles(type, disabled)} ${className} `}
       disabled={disabled}
     >
       {children}
@@ -49,7 +50,9 @@ export const ButtonLink = ({
 }: React.PropsWithChildren<ButtonLinkProps>) => {
   return (
     <NextLink passHref href={href}>
-      <button className={` ${ButtonSwitchStyles(type)} ${className}`}>
+      <button
+        className={cls(defaultStyles.base, buttonStyles[type], { className })}
+      >
         {children}
       </button>
     </NextLink>
