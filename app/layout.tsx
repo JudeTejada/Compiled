6ed82796415666cb281da823b64085f30986a85e
Jude Tejada <judelaurentetejada@gmail.com>
2023-03-15 22:03:57 +0800

    temp type fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,13 @@ import { Sidebar, Container, Header, Button } from 'app/components';
 import { Inter } from 'next/font/google';
 import { ChevronUpIcon } from '@heroicons/react/24/outline';
 import { useScrollToBottom } from 'hooks';
-import { type ReactNode, useRef } from 'react';
+import { useRef, PropsWithChildren, ReactChild } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
-function MyApp(props: { children: ReactChild }) {
+type RootLayoutProps = PropsWithChildren;
+
+function RootLayout(props: RootLayoutProps) {
   const { children } = props;
   const mainRef = useRef<HTMLElement>(null);
 
@@ -23,35 +25,38 @@ function MyApp(props: { children: ReactChild }) {
   const baseUrl = process.env.NEXT_PUBLIC_HOST;
 
   return (
-    <html>
-      <head>
-        <meta property='og:image' content={`${baseUrl}/api/og`} />
-      </head>
-      <body className={inter.className}>
-        <RecoilRoot>
-          <Sidebar />
-          <main className='flex min-h-screen relative' ref={mainRef}>
-            <Container>
-              <Header />
-              {children}
+    <>
+      <html>
+        <head>
+          <meta property='og:image' content={`${baseUrl}/api/og`} />
+        </head>
+        <body className={inter.className}>
+          <RecoilRoot>
+            <Sidebar />
+            <main className='flex min-h-screen relative' ref={mainRef}>
+              <Container>
+                <Header />
 
-              {reachedBottom && (
-                <Button
-                  type='primary'
-                  className='rounded-full fixed right-5 bottom-5  w-12 shadow-xl'
-                  onClick={() =>
-                    window.scrollTo({ top: 0, behavior: 'smooth' })
-                  }
-                >
-                  <ChevronUpIcon />
-                </Button>
-              )}
-            </Container>
-          </main>
-        </RecoilRoot>
-      </body>
-    </html>
+                {children as ReactChild}
+
+                {reachedBottom && (
+                  <Button
+                    type='primary'
+                    className='rounded-full fixed right-5 bottom-5  w-12 shadow-xl'
+                    onClick={() =>
+                      window.scrollTo({ top: 0, behavior: 'smooth' })
+                    }
+                  >
+                    <ChevronUpIcon />
+                  </Button>
+                )}
+              </Container>
+            </main>
+          </RecoilRoot>
+        </body>
+      </html>
+    </>
   );
 }
 
-export default MyApp;
+export default RootLayout;
